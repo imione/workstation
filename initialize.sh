49fa086d06ce776a7c7935fb9cfd32388e865652
김수현 <gimsuhyeon@gimsuhyeon-ui-MacBookPro.local>
2022-09-23 11:13:33 +0900

    initial commit

---
+++ initialize.sh
@@ -0,0 +1,20 @@
+# install homebrew
+$ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+$ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/gimsuhyeon/.zprofile
+$ eval "$(/opt/homebrew/bin/brew shellenv)"
+$ brew help
+
+# install text editor
+$ brew install --cask sublime-text
+
+# install slack
+$ brew install --cask slack
+
+# install iterm2
+$ brew install --cask iterm2
+
+# install 1password
+$ brew install --cask 1password
+
+# install notion
+$ brew install --cask notion
