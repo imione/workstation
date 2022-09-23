34f837da704efbdf0552953e39589dbfe7c9c48c
김수현 <gimsuhyeon@gimsuhyeon-ui-MacBookPro.local>
2022-09-24 00:10:09 +0900

    feat: zsh, nvm, vscode 설치 스크립트 추가

--- initialize.sh
+++ initialize.sh
@@ -18,3 +18,32 @@ brew install --cask 1password
 
 # install notion
 brew install --cask notion
+
+# install zsh
+brew install zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install typewritten theme https://github.com/reobin/typewritten
+git clone https://github.com/reobin/typewritten.git $ZSH_CUSTOM/themes/typewritten
+ln -s "$ZSH_CUSTOM/themes/typewritten/typewritten.zsh-theme" "$ZSH_CUSTOM/themes/typewritten.zsh-theme"
+ln -s "$ZSH_CUSTOM/themes/typewritten/async.zsh" "$ZSH_CUSTOM/themes/async"
+
+# install zsh-syntax-highlighting
+brew install zsh-syntax-highlighting
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+
+# install nvm
+mkdir ~/.nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+brew install nvm
+nvm install v16
+
+
+# install vscode
+brew install --cask visual-studio-code
+
