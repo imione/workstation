86d98008fabf36b6b6853e2b629d0d1c1ccb2723
김수현 <gimsuhyeon@ip-192-168-35-102.ap-northeast-2.compute.internal>
2022-10-20 11:46:51 +0900

    feat: aws cli 설치 스크립트 추가

--- initialize.sh
+++ initialize.sh
@@ -61,3 +61,9 @@ brew install --cask cron
 
 # install postman
 brew install --cask postman
+
+# install aws cli
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg ./AWSCLIV2.pkg -target /
+# 설치 경로 확인: which aws
+# 버전 확인: aws --version
